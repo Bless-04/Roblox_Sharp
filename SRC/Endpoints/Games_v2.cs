9b8731fcc8857d555bce1245a2fa85d8aae93757
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-11-26 13:07:51 -0500

    Games Endpoint Added

---
+++ SRC/Endpoints/Games_v2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roblox_Sharp.Endpoints
+{
+    internal class Games_v2
+    {
+    }
+}
