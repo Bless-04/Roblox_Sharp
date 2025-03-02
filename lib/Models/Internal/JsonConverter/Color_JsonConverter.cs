3c52372785fce9cd4dcb7d06d07b52decf72c211
Blessing Abumere <96212794+Bless-04@users.noreply.github.com>
2025-03-02 00:37:27 -0500

    Update Color_JsonConverter.cs
    Signed-off-by: Blessing Abumere <96212794+Bless-04@users.noreply.github.com>

--- lib/Models/Internal/JsonConverter/Color_JsonConverter.cs
+++ lib/Models/Internal/JsonConverter/Color_JsonConverter.cs
@@ -8,7 +8,7 @@ namespace Roblox_Sharp.Models.Internal.JsonConverter
     /// <summary>
     /// used to convert the color to and from RGB hex
     /// </summary>
-    internal sealed class Color_JsonConverter : JsonConverter<Color>
+    sealed internal class Color_JsonConverter : JsonConverter<Color>
     {
         public override Color Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
            ColorTranslator.FromHtml(
