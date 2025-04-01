83719a154acd80c9456127b3ca7e3fc1622482ee
Blessing <babumere04@gmail.com>
2025-04-01 16:14:54 -0400

    3.0.0 in progress

---
+++ xUnitTests/IntegrationTrait.RateLimitted.FailCase2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace xUnitTests;
+public partial class RateLimitted
+{
+        /// <summary>
+        /// Same as <see cref="FailCase"/> but for tests that are easily rate limited
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        public sealed class FailCase2(string name = nameof(Roblox_Sharp.Endpoints), string value =nameof(FailCase2)) : RateLimitted;
+}
--- xUnitTests/IntegrationTrait.RateLimitted.cs
+++ xUnitTests/IntegrationTrait.RateLimitted.cs
@@ -11,7 +11,7 @@ public partial class IntegrationTrait
     /// </summary>
     [TraitDiscoverer("Xunit.Sdk.TraitDiscoverer", "xunit.core")]
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    public class RateLimitted(string name = nameof(Roblox_Sharp.Endpoints), string value = nameof(RateLimitted)) : BeforeAfterTestAttribute,
+    public partial class RateLimitted(string name = nameof(Roblox_Sharp.Endpoints), string value = nameof(RateLimitted)) : BeforeAfterTestAttribute,
         ITraitAttribute
     {
         /// <summary>
@@ -20,12 +20,5 @@ public partial class IntegrationTrait
         public const ushort Delay = 61000;
 
         public override void Before(MethodInfo methodUnderTest) => Task.Delay(Delay).Wait();
-        
-        /// <summary>
-        /// Same as <see cref="FailCase"/> but for tests that are easily rate limited
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="value"></param>
-        public sealed class FailCase2(string name = nameof(Roblox_Sharp.Endpoints), string value =nameof(FailCase2)) : RateLimitted;
     }
 }
