a1a3676b9daeb29c643f540bffaa10e7fca1b789
Blessing <babumere04@gmail.com>
2025-04-01 20:20:43 -0400

    Update IntegrationTrait.RateLimitted.FailCase2.cs

--- xUnitTests/IntegrationTrait.RateLimitted.FailCase2.cs
+++ xUnitTests/IntegrationTrait.RateLimitted.FailCase2.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Reflection;
-using System.Threading.Tasks;
-using Xunit.Sdk;
-
 namespace xUnitTests;
-public partial class RateLimitted
+public partial class IntegrationTrait
 {
+    public partial class RateLimitted
+    {
         /// <summary>
         /// Same as <see cref="FailCase"/> but for tests that are easily rate limited
         /// </summary>
         /// <param name="name"></param>
         /// <param name="value"></param>
-        public sealed class FailCase2(string name = nameof(Roblox_Sharp.Endpoints), string value =nameof(FailCase2)) : RateLimitted;
+        public sealed class FailCase2(string name = nameof(Roblox_Sharp.Endpoints), string value = nameof(FailCase2)) : RateLimitted;
+    }
 }
