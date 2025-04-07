42719e74e14913fc7f0a405eec5335563033e55b
Blessing <babumere04@gmail.com>
2025-04-07 09:25:30 -0400

    Update User.cs
    simplification

--- src/Abstractions/User.cs
+++ src/Abstractions/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Reflection.Metadata;
 using System.Text.Json.Serialization;
 
 namespace Roblox_Sharp.Abstractions
@@ -19,7 +20,7 @@ namespace Roblox_Sharp.Abstractions
     /// abstract class for models representing User information that have properties containing both the unique user id and the unique usernae
     /// </summary>
     [DebuggerDisplay("{Username} (ID {UserId})")]
-    public abstract class User : Creation<IUser>, IUser, IFormattable
+    public abstract class User : Creation<IUser>, IUser
     {
         /// <summary>
         /// The <see cref="StringComparer"/> used to compare the <see cref="User.Username"/>
@@ -45,36 +46,5 @@ namespace Roblox_Sharp.Abstractions
         /// A string representation of the <see cref="User"/>
         /// </returns>
         public override string ToString() => $"{Username} (ID {UserId})";
-
-        /// /// <summary>
-        /// Formats the <see cref="User"/> information based on the provided format string.
-        /// Supported format strings:
-        /// <list type="table">
-        ///   <listheader>
-        ///     <term>Format</term>
-        ///     <description>Output</description>
-        ///   </listheader>
-        ///   <item>
-        ///     <term>"id"</term>
-        ///     <description>Returns the <see cref="User.UserId"/></description>
-        ///   </item>
-        ///   <item>
-        ///     <term>"name"</term>
-        ///     <description>Returns the <see cref="User.Username"/></description>
-        ///   </item>
-        /// </list>
-        /// </summary>
-        /// <returns>
-        /// <inheritdoc cref="IFormattable.ToString(string?, IFormatProvider?)"/>
-        /// </returns>
-        public string ToString(string? format, IFormatProvider? formatProvider) => format == null ? ToString()
-            : format switch
-            {
-                //user id 
-                "id" => $"(ID {UserId}) ",
-                "name" => "@{username} ",
-                _ => throw new FormatException()
-            };
-
     }
 }
